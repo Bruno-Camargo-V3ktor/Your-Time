da350e79513d8b456d6607d3d21fdce6c4c02eb3
Bruno C. <v3ktoryt@gmail.com>
2024-09-04 22:12:51 -0300

    ✨ feat: 'Adicinando o ThemeProvider em nossa aplicação e criando o arquvio das configurações do tema padrão'

--- src/styles/Button.styles.ts
+++ src/styles/Button.styles.ts
@@ -20,7 +20,12 @@ const buttonVariants = {
 
 export const ButtonContainer = styled.button<ButtonContainerProps>`
     width: 100px;
-    height: 100px;
+    height: 40px;
+    border-radius: 8px;
+    border: 0;
+    margin: 8px;
 
-    ${ props => { return css`background-color: ${buttonVariants[props.variant]}`  }  }
+    background-color: ${ (props) => props.theme.primary };
+    color: ${ (props) => props.theme.white }
+    //${ props => { return css`background-color: ${buttonVariants[props.variant]}`  }  }
 `
